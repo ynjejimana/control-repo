604ab2a2dd5184ab0e2de2fad96755aa045a121c
Yves <njejiyves2005@yahoo.fr>
2022-02-02 21:18:38 +1100

    updating node file

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,3 +2,16 @@ node puppet.lab.com {
   include role::master
 }
 
+node 'agent1.lab.com' {
+  package { 'vim' :
+    ensure => present,
+  }
+}
+
+
+node 'agent2.lab.com' {
+  package { 'vim' :
+    ensure => present,
+  }
+}
+
