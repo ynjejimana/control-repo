53cabc0c8de3e290fc5927e6773c1e6b5fa332e7
Yves Njejimana <njejiyves2005@yahoo.fr>
2023-02-18 01:10:06 +1100

    updating apatche

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -1,4 +1,4 @@
 class apache {
   package { 'httpd': ensure => installed }
-  service { 'httpd': ensure => true, enable => true, require => Package['httpd'] }
+  service { 'httpd': ensure => true, enable => true, hasstatus => false, require => Package['httpd'] }
 }
