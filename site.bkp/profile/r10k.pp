9fece675a8791ea22932dd0c1973971b2c65f5ea
Yves Njejimana <njejiyves2005@yahoo.fr>
2023-02-21 21:04:36 +1100

    updating class

---
+++ site.bkp/profile/r10k.pp
@@ -0,0 +1,14 @@
+class profile::r10k {
+  class {'r10k':
+    remote => 'https://github.com/ynjejimana/control-repo',
+  }
+  class {'r10k::webhook::config':
+    use_mcollective => false,
+    enable_ssl      => false,
+  }
+  class {'r10k::webhook':
+    user  =>  'root',
+    group =>  'root',
+  }
+}
+
