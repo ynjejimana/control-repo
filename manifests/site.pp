b87756b7d3a2ea7dd0f05e3cc0320be43955a3eb
Yves Njejimana <njejiyves2005@yahoo.fr>
2023-02-23 20:33:37 +1100

    Update v.00003

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,21 +1,21 @@
-class nodes:: {
+#class nodes {
 
-include common::motd::default
-include common::users::dev_group::group_name
-include common::users::net_group::group_name
-include common::users::uni_group::group_name
-include common::users::recovery_account::password
-include common::users::root_account::password
-include common::users::admin_group::group_name
-include common::users::unix_accounts::current_users
-include common::users::unix_accounts::former_users
-include common::users::developers_accounts::former_users
-include common::users::developers_accounts::current_users
-include common::users::networking_accounts::current_users
-include common::users::networking_accounts::former_users
+#include common::motd::default
+#include common::users::dev_group::group_name
+#include common::users::net_group::group_name
+#include common::users::uni_group::group_name
+#include common::users::recovery_account::password
+#include common::users::root_account::password
+#include common::users::admin_group::group_name
+#include common::users::unix_accounts::current_users
+#include common::users::unix_accounts::former_users
+#include common::users::developers_accounts::former_users
+#include common::users::developers_accounts::current_users
+#include common::users::networking_accounts::current_users
+#include common::users::networking_accounts::former_users
 # include common::resolv::resolvconf::search_domains
 
-}
+#}
 
 #node 'agent01.lab.com' {
 #        include apache
