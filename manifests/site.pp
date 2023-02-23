b6a69394eed7101fe905d99073c1747baf19d43b
Yves Njejimana <njejiyves2005@yahoo.fr>
2023-02-24 00:15:55 +1100

    Update v.00009

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,6 @@
 node 'agent01.lab.com' {
 
-	include common::users::developers_accounts::current_users
+	include common::users::developers_accounts
 	include common::users::dev_group
 	include common::motd::default
 
